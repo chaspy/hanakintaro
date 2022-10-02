db328bf366ebefa04c5eb89b294fda89e95d5edf
kondo takeshi <chaspy@users.noreply.github.com>
2022-10-02 14:37:53 +0900

    Add scope

--- manifest.ts
+++ manifest.ts
@@ -1,5 +1,5 @@
-import { Manifest } from "deno-slack-sdk/mod.ts";
-import GreetingWorkflow from "./workflows/greeting_workflow.ts";
+import { Manifest } from 'deno-slack-sdk/mod.ts'
+import GreetingWorkflow from './workflows/greeting_workflow.ts'
 
 /**
  * The app manifest contains the app's configuration. This
@@ -7,11 +7,16 @@ import GreetingWorkflow from "./workflows/greeting_workflow.ts";
  * https://api.slack.com/future/manifest
  */
 export default Manifest({
-  name: "hanakin",
+  name: 'hanakin',
   description:
-    "A sample that demonstrates using a function, workflow and trigger to send a greeting",
-  icon: "assets/icon.png",
+    'A sample that demonstrates using a function, workflow and trigger to send a greeting',
+  icon: 'assets/icon.png',
   workflows: [GreetingWorkflow],
   outgoingDomains: [],
-  botScopes: ["commands", "chat:write", "chat:write.public"],
-});
+  botScopes: [
+    'commands',
+    'chat:write',
+    'chat:write.public',
+    'app_mentions:read',
+  ],
+})
