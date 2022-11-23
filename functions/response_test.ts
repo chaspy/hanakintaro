150101422d8f1270ea62f0ce2823a7344e2c00da
kondo takeshi <chaspy@users.noreply.github.com>
2022-11-23 18:15:37 +0900

    Add test

--- functions/response_test.ts
+++ functions/response_test.ts
@@ -16,6 +16,28 @@ Deno.test('Response function test -- keyword', async () => {
   )
 })
 
+// when: @hanakin keyword timezone
+// expect: return answer
+Deno.test('Response function test -- keyword with timezone', async () => {
+  const inputs = { message: `<@ABCDEFGHIJK> ${env.answer} JST` }
+  const { outputs } = await ResponseFunction(createContext({ inputs }))
+  assertEquals(
+    Object.values(env.message).includes(`${outputs?.response}`),
+    true
+  )
+})
+
+// when: @hanakin keyword invalid-timezone
+// expect: return error message
+Deno.test(
+  'Response function test -- keyword with invalid timezone',
+  async () => {
+    const inputs = { message: `<@ABCDEFGHIJK> ${env.answer} ABC` }
+    const { outputs } = await ResponseFunction(createContext({ inputs }))
+    assertEquals(`${outputs?.response}`, 'ABC is invalid timezone')
+  }
+)
+
 // when: @hanakin non-keyword
 // expect: return usage
 Deno.test('Response function test -- non-keyword', async () => {
